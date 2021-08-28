f143610a29fba391b21bc4f259182d927b8773f8
RGeetha24 <rgeetha2296@gmail.com>
2021-08-28 08:10:23 +0530

    Update Designation.php

--- app/Http/Controllers/Designation.php
+++ app/Http/Controllers/Designation.php
@@ -12,4 +12,50 @@ class Designation extends Controller
         return view('admin.designation.index',compact('designation'));
 
     }
+    public function descadd(Request $request)
+    {
+        $designation = new ModelsDesignation;
+        $designation->name = $request->input('name');
+
+        $this->validate(
+            $request,
+                [
+
+                    'name'=>'required',
+                ],
+                [
+
+                    'name.required'=>'Designation Name Required',
+
+                ]
+        );
+        $designation->save();
+        return redirect('/designation')->with('status','Designation added successfully');
+
+    }
+
+    public function edit($id)
+    {
+        $designation = ModelsDesignation::findOrFail($id);
+
+        return view('admin.designation.edit',compact('designation'));
+
+    }
+
+    public function update(Request $request,$id)
+    {
+        $designation = ModelsDesignation::find($id);
+        $designation->name = $request->input('name');
+        $designation->update();
+        return redirect('/designation')->with('status','Your data is updated');
+
+    }
+
+    public function delete($id)
+    {
+        $designation = ModelsDesignation::findOrFail($id);
+        $designation->delete();
+
+        return redirect('/designation')->with('status','Your data is deleted');
+    }
 }
