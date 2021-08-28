c29dfbdb22b6d221e3ea76b9fb43b0b1e1bc7b74
RGeetha24 <rgeetha2296@gmail.com>
2021-08-28 08:09:58 +0530

    Update Deparment.php

--- app/Http/Controllers/Deparment.php
+++ app/Http/Controllers/Deparment.php
@@ -13,4 +13,51 @@ class Deparment extends Controller
         return view('admin.department.index',compact('department'));
 
     }
+
+    public function deptadd(Request $request)
+    {
+        $department = new ModelsDepartment;
+        $department->name = $request->input('name');
+
+        $this->validate(
+            $request,
+                [
+
+                    'name'=>'required',
+                ],
+                [
+
+                    'name.required'=>'Department Name Required',
+
+                ]
+        );
+        $department->save();
+        return redirect('/department')->with('status','Department added successfully');
+
+    }
+
+    public function edit($id)
+    {
+        $department = ModelsDepartment::findOrFail($id);
+
+        return view('admin.department.edit',compact('department'));
+
+    }
+
+    public function update(Request $request,$id)
+    {
+        $department = ModelsDepartment::find($id);
+        $department->name = $request->input('name');
+        $department->update();
+        return redirect('/department')->with('status','Your data is updated');
+
+    }
+
+    public function delete($id)
+    {
+        $department = ModelsDepartment::findOrFail($id);
+        $department->delete();
+
+        return redirect('/department')->with('status','Your data is deleted');
+    }
 }
